7dd47cc4070462dce072712a9cdfa70819aeb060
Kate Dameron <kater@Kates-MacBook-Pro.local>
2020-03-21 14:54:01 -0700

    Set up header and delete CRA default files - favicon

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>S
+          <span className="flicker-fast">T</span>
+          <span className="flicker-slow">R</span>
+          ANGER THINGS 
+          <span className="flicker-slow"> A</span>
+          PI TUTOR
+          <span className="flicker-fast">I</span>
+          <span className="flicker-slow">AL</span>
+        </h1>
       </header>
     </div>
   );
