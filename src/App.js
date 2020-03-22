9481697501102074c157eb49a47133e11ac32ce6
Kate Dameron <kater@Kates-MacBook-Pro.local>
2020-03-22 15:48:01 -0700

    add divider lines

--- src/App.js
+++ src/App.js
@@ -21,10 +21,12 @@ function App() {
         <h2>Prerequisites</h2>
         <p>This project uses Node.js, Express, Superagent, MongoDB, Mongoose, and node-html-parser deployed to Heroku. This tutorial requires some familiarity with Node.js, Express, and MongoDB, but we have linked resources for you as well. Other technologies are available!</p>
       </section>
+      <div className="neon-divider"></div>
       <section>
         <h2>Getting set up</h2>
         <p>You'll need to set up a <a href="https://nodejs.org/en/docs/guides/getting-started-guide/">Node.js server</a>. We used <a href="https://expressjs.com/en/starter/hello-world.html">Express.js</a> but you can use whatever you want! We're not going to get into the details of setting up Node/Express. The docs provide great instructions and there are free tutorials available. You can also find the source code for this entire api tutorial <a href="https://github.com/api-tutorial/api-tutorial">HERE</a>.</p>
       </section>
+      <div className="neon-divider flicker-slow"></div>
       <section>
         <h2>Things to consider before scraping</h2>
         <ul>
@@ -33,6 +35,7 @@ function App() {
           <li>Use your browser devtools to inspect the elements in the page that contain the data you're trying to scrape. Does it have have selectors that will be easy to target when trying to retreive the data? (more about this is in<a href="#step-2"> step 2</a>.</li>
         </ul>
       </section>
+      <div className="neon-divider"></div>
       <section>
         <h2>Step 1: Time to <s>hack into</s> scape some data</h2>
         <ol>
@@ -47,25 +50,26 @@ function App() {
             <div className="code-block"> 
             <pre><code>{` const request = require('superagent');
  const { parse } = require('node-html-parser');
-
+ 
  const scraper = () => {
    return request
    .get([your url here])
    .then(res => res.text)
    .then(parse)
    .then(console.log);
- };
-
- scraper();
-
- module.exports = { scraper };
-`}</code></pre>
+  };
+  
+  scraper();
+  
+  module.exports = { scraper };
+  `}</code></pre>
             </div>
             <p>Now you should be able to run <code>node scraper.js</code> and see some html data appear in your console.</p>
           </li>
         </ol>
       </section>
-      <section>
+      <div className="neon-divider"></div>
+      <section id="step-2">
         <h2>Step 2: Make a game plan</h2>
         <p>Open up your dev tools and inspect the elements that hold the data you need to scrape. In our case, the first thing we needed to do was grab each <code>h2</code> header with the class <code>pi-title</code>.</p>
         <div><img src={require('./assets/inspectView.png')} alt="screenshot showing stranger things wiki fan site with browser devtools open inspecting html elements"/></div>
@@ -89,6 +93,7 @@ function App() {
       <p>run <code>node scraper.js</code> again</p>
       <p>At this point you should be able to see the data and start to make decisions about how to grab different elements, run some clean up functions and start to piece it all together to match your db schema.</p>
       </section>
+      <div className="neon-divider flicker-slow"></div>
     </main>
   </div>
   );
