cd600e1a1f4448aed14f41f89284b59fc0253fc3
Kate Dameron <kater@Kates-MacBook-Pro.local>
2020-05-02 16:18:49 -0700

    change up colors on menu

--- src/drawer/Button.js
+++ src/drawer/Button.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Button } from "antd";
 import MenuOpenIcon from "@material-ui/icons/MenuOpen";
+import KeyboardArrowLeftIcon from "@material-ui/icons/KeyboardArrowLeft";
 import "../App.css";
 
 const MenuButton = ({ handleClick }) => (
   <Button aria-label="open menu" type="primary" onClick={handleClick}>
-    <MenuOpenIcon />
+    {/* <MenuOpenIcon /> */}
+    <KeyboardArrowLeftIcon />
   </Button>
 );
 
