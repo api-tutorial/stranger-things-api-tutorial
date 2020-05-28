cb4a653f2ad47157f0c835ed5d997a0629698a76
Kate Dameron <kater@Kates-MacBook-Pro.local>
2020-05-27 20:36:45 -0700

    hover and panel styles

--- src/pages/Documentation.js
+++ src/pages/Documentation.js
@@ -1,149 +1,143 @@
-import React from "react";
-import "../App.css";
+import React, { useState } from "react";
 import { Divider } from "../components";
 import Sandbox from "../Sandbox";
-import data from '../data/example-data.json';
-import { 
-  ExpansionPanel, 
-  ExpansionPanelSummary, 
-  ExpansionPanelDetails 
-} from '@material-ui/core'
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+import data from "../data/example-data.json";
+import {
+  ExpansionPanel,
+  ExpansionPanelSummary,
+  ExpansionPanelDetails,
+} from "@material-ui/core";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import "../App.css";
 
 const panelStyle = {
-  background: 'black',
-  color: 'white',
+  background: "black",
+  color: "#d9d9d9",
 };
 
-const expandIcon = <ExpandMoreIcon style={{color: 'white'}} />
+const expandIcon = <ExpandMoreIcon style={{ color: "white" }} />;
 
-const Documentation = () => (
-  <>
-    <section className="endpoints">
-      <h3>Endpoints</h3>
-      <p>stranger-things-api.herokuapp.com/</p>
-      <ul>
-        <li>
-          <ExpansionPanel
-            classes="endpoints-list"
-            style={panelStyle}
-          >
-            <ExpansionPanelSummary
-              expandIcon={expandIcon}
-              aria-controls="panel1a-content"
-              id="panel1a-header"
-              classes="endpoints-list"
-            >
-              <span className="bold">GET all characters</span> /api/v1/characters
-            </ExpansionPanelSummary>
-            <ExpansionPanelDetails
-              className="example-json"
-            >
-              <pre>
-                {JSON.stringify(data.all, null, 1)}
-              </pre>
-            </ExpansionPanelDetails>
-          </ExpansionPanel>
-        </li>
-        <li>
+const Documentation = () => {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
+
+  return (
+    <>
+      <section className="endpoints">
+        <h3>Endpoints</h3>
+        <p>stranger-things-api.herokuapp.com/</p>
+        <ul className="endpoints-list">
+          <li>
             <ExpansionPanel
-            classes="endpoints-list"
-            style={panelStyle}
-          >
-            <ExpansionPanelSummary
-              classes="endpoints-list"
-              expandIcon={expandIcon}
-              aria-controls="panel1a-content"
-              id="panel1a-header"
+              style={panelStyle}
+              expanded={expanded === "panel1"}
+              onChange={handleChange("panel1")}
             >
-              <span className="bold">GET by ID</span> /api/v1/characters/:id
-            </ExpansionPanelSummary>
-            <ExpansionPanelDetails
-              className="example-json"
-            >
-              <pre>
-                {JSON.stringify(data.id, null, 1)}
-              </pre>
-            </ExpansionPanelDetails>
-          </ExpansionPanel>
-        </li>
-        <li>
-          <ExpansionPanel
-            style={panelStyle}
-          >
-            <ExpansionPanelSummary
-              classes="endpoints-list"
-              expandIcon={expandIcon}
-              aria-controls="panel1a-content"
-              id="panel1a-header"
-            >
-              <span className="bold">GET random character(s)</span> /api/v1/characters/random?count=1
-            </ExpansionPanelSummary>
-            <ExpansionPanelDetails
-              className="example-json"
-            >
-              <pre>
-                {JSON.stringify(data.random, null, 1)}
-              </pre>
-            </ExpansionPanelDetails>
-          </ExpansionPanel>
-        </li>
-        <li>
-
-          <ExpansionPanel
-            style={panelStyle}
-          >
-            <ExpansionPanelSummary
-                classes="endpoints-list"
+              <ExpansionPanelSummary
                 expandIcon={expandIcon}
                 aria-controls="panel1a-content"
                 id="panel1a-header"
               >
-                <span className="bold">GET by query</span> /api/v1/characters?name=Mike+Wheeler
+                <span className="bold">GET all characters</span>{" "}
+                /api/v1/characters
               </ExpansionPanelSummary>
-              <ExpansionPanelDetails
-                className="example-json"
+              <ExpansionPanelDetails className="example-json">
+                <pre>{JSON.stringify(data.all, null, 1)}</pre>
+              </ExpansionPanelDetails>
+            </ExpansionPanel>
+          </li>
+          <li>
+            <ExpansionPanel
+              style={panelStyle}
+              expanded={expanded === "panel2"}
+              onChange={handleChange("panel2")}
+            >
+              <ExpansionPanelSummary
+                expandIcon={expandIcon}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
               >
-                <div>
+                <span className="bold">GET by ID</span> /api/v1/characters/:id
+              </ExpansionPanelSummary>
+              <ExpansionPanelDetails className="example-json">
+                <pre>{JSON.stringify(data.id, null, 1)}</pre>
+              </ExpansionPanelDetails>
+            </ExpansionPanel>
+          </li>
+          <li>
+            <ExpansionPanel
+              style={panelStyle}
+              expanded={expanded === "panel3"}
+              onChange={handleChange("panel3")}
+            >
+              <ExpansionPanelSummary
+                expandIcon={expandIcon}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+              >
+                <span className="bold">GET random character(s)</span>{" "}
+                /api/v1/characters/random?count=1
+              </ExpansionPanelSummary>
+              <ExpansionPanelDetails className="example-json">
+                <pre>{JSON.stringify(data.random, null, 1)}</pre>
+              </ExpansionPanelDetails>
+            </ExpansionPanel>
+          </li>
+          <li>
+            <ExpansionPanel
+              style={panelStyle}
+              expanded={expanded === "panel4"}
+              onChange={handleChange("panel4")}
+            >
+              <ExpansionPanelSummary
+                expandIcon={expandIcon}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+              >
+                <span className="bold">GET by query</span>{" "}
+                /api/v1/characters?name=Mike+Wheeler
+              </ExpansionPanelSummary>
+              <ExpansionPanelDetails className="example-json">
+                <div className="queries-box">
                   <h3>Available Queries</h3>
                   <p>
                     aliases, otherRelations, affiliation, occupation, residence,
-                    appearsInEpisodes, status, gender, eyeColor, born, hairColor,
-                    portrayedBy
+                    appearsInEpisodes, status, gender, eyeColor, born,
+                    hairColor, portrayedBy
                   </p>
                 </div>
-                <pre>
-                  {JSON.stringify(data.name, null, 1)}
-                </pre>
+                <pre>{JSON.stringify(data.name, null, 1)}</pre>
               </ExpansionPanelDetails>
-          </ExpansionPanel>
-        </li>
-        <li>
-          <ExpansionPanel
-            style={panelStyle}
-          >
-            <ExpansionPanelSummary
-              classes="endpoints-list"
-              expandIcon={expandIcon}
-              aria-controls="panel1a-content"
-              id="panel1a-header"
+            </ExpansionPanel>
+          </li>
+          <li>
+            <ExpansionPanel
+              style={panelStyle}
+              expanded={expanded === "panel5"}
+              onChange={handleChange("panel5")}
             >
-              <span className="bold">GET all with pagination</span> /api/v1/characters?perPage=5?page=1
-            </ExpansionPanelSummary>
-            <ExpansionPanelDetails
-              className="example-json"
-            >
-              <pre>
-                {JSON.stringify(data.pagination, null, 1)}
-              </pre>
-            </ExpansionPanelDetails>
-          </ExpansionPanel>
-        </li>
-      </ul>
-    </section>
-    <Divider />
-    <Sandbox />
-  </>
-);
+              <ExpansionPanelSummary
+                expandIcon={expandIcon}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+              >
+                <span className="bold">GET all with pagination</span>{" "}
+                /api/v1/characters?perPage=5?page=1
+              </ExpansionPanelSummary>
+              <ExpansionPanelDetails className="example-json">
+                <pre>{JSON.stringify(data.pagination, null, 1)}</pre>
+              </ExpansionPanelDetails>
+            </ExpansionPanel>
+          </li>
+        </ul>
+      </section>
+      <Divider />
+      <Sandbox />
+    </>
+  );
+};
 
 export default Documentation;
