3f96e7771cfcf30cdda3746972e76e0b8fb58ef5
Kate Dameron <kater@Kates-MacBook-Pro.local>
2020-05-30 13:28:40 -0700

    fixes for code blocks

--- src/Sandbox.js
+++ src/Sandbox.js
@@ -2,12 +2,11 @@ import React, { useState } from "react";
 
 const Sandbox = () => {
   const [userInput, setUserInput] = useState("");
-  const [data, setData] = useState(
-    `Finish the URL /api/v1/characters... to try different queries`
-  );
+  const [data, setData] = useState([]);
   const API_URL = "https://stranger-things-api.herokuapp.com/api/v1/characters";
 
-  const fetchData = () => {
+  const fetchData = (e) => {
+    e.preventDefault();
     return fetch(`${API_URL}/${userInput}`)
       .then((res) => res.json())
       .then(setData)
@@ -20,16 +19,18 @@ const Sandbox = () => {
         <fieldset>
           <legend>Try out an endpoint:</legend>
           <div id="user-input">
-            <label htmlFor="endpoint-input">/api/v1/characters</label>
+            <label htmlFor="endpoint-input">
+              Finish the URL /api/v1/characters/
+            </label>
             <input
               id="endpoint-input"
               placeholder="example: /random"
               value={userInput}
               onChange={({ target }) => setUserInput(target.value)}
-              aria-label="Try out an endpoint by finishing the url /api/v1/characters"
+              aria-label="Try out an endpoint by finishing the url /api/v1/characters/"
             />
-            <button id="submit-btn" onClick={() => fetchData()}>
-              Try It
+            <button id="submit-btn" onClick={(e) => fetchData(e)}>
+              Fetch
             </button>
           </div>
         </fieldset>
