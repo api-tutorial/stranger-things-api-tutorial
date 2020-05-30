ba112f57d329fe518f5d9ec81043331c19d7d793
Kate Dameron <kater@Kates-MacBook-Pro.local>
2020-05-30 15:04:00 -0700

    remove Tutorial from mobile header

--- src/title/MobileTitle.js
+++ src/title/MobileTitle.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./title.css";
 
-const Title = () => <h1>Stranger Things API Tutorial</h1>;
+const Title = () => <h1>Stranger Things API</h1>;
 
 export default Title;
